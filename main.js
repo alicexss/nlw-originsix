75ddd8dfe5e71215022a4bf9811c71bb9f14e4f8
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-06-23 16:46:30 -0300

    adding new session

---
+++ main.js
@@ -0,0 +1,19 @@
+/*Abre e fecha o menu quando clicar no icone*/
+
+const nav = document.querySelector('header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+/*Quando clicar em algum item do menu, esconder o menu */
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
