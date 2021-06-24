0fb7bdd4284a5519baccfdc5e4f8aaf21ee5e402
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-06-23 22:03:20 -0300

    add menu scroll

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /*Abre e fecha o menu quando clicar no icone*/
 
-const nav = document.querySelector('header nav')
+const nav = document.querySelector('#header nav')
 const toggle = document.querySelectorAll('nav .toggle')
 
 for (const element of toggle) {
@@ -17,3 +17,15 @@ for (const link of links) {
     nav.classList.remove('show')
   })
 }
+
+/* mudar o header da pagina quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
