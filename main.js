2e986dc0ed5254f0e021128614f57f6998c19679
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-06-25 11:34:24 -0300

    add back to top button

--- main.js
+++ main.js
@@ -55,8 +55,20 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card, 
   #testimonials header, #testimonials .testimonials
-  #contact .text, #contact .links
+  #contact .text, #contact .links,
+  footer .brand, footer .social
   
   `,
   { interval: 100 }
 )
+
+/* Botao voltar para o topo */
+
+const backToTopButton = document.querySelector('.back-to-top')
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+})
