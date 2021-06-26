7ce2eea3bcba2ed78f258faa9f1326c786c21317
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-06-26 12:46:23 -0300

    add link ativo

--- main.js
+++ main.js
@@ -19,10 +19,10 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
+function changeHeaderWhenScroll() {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -73,14 +73,39 @@ scrollReveal.reveal(
 )
 
 /* Botao voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
 
 function bachToTop() {
-  const backToTopButton = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopButton.classList.add('show')
   } else {
     backToTopButton.classList.remove('show')
+  }
+}
+
+/* Menu ativo conforme a sessao visível na pagina */
+const sections = document.querySelectorAll('main section[id]')
+
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
   }
 }
 
@@ -89,4 +114,5 @@ function bachToTop() {
 window.addEventListener('scroll', function () {
   changeHeaderWhenScroll()
   bachToTop()
+  activateMenuAtCurrentSection()
 })
