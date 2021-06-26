2c9f68649bdf19c26957b619ae2195d5557d74f8
Alice Xavier dos Santos <alicexaviers21@outlook.com>
2021-06-25 23:08:10 -0300

    finish responsive

--- main.js
+++ main.js
@@ -42,7 +42,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* Mostrar elementos quando der scroll na pagina */
